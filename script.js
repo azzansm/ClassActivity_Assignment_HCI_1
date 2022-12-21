2ea3f51eba132e1093ec2fea4b869c578b1f0b46
azzansm <89733998+azzansm@users.noreply.github.com>
2022-12-21 14:44:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //Please put your answer here
+$('.fa-times').click(function(){
+    $('.signup-modal-wrapper').hide();
+});
+
+$('.btn').click(function(){
+    $('.signup-modal-wrapper').show();
+});
